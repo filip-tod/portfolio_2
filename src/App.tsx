d9a9484cae64e5ca324ccc16a2572a5c93faa6ea
billybord <filip.tod95@gmail.com>
2025-07-24 19:56:13 +0200

    Initial commit - setup project structure and routing

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>
+      <h1>Welcome to My Portfolio</h1>
+      </header>
+    
+      <div>
+      <p>This is a simple portfolio application built with React.</p>
+      <p>Explore my projects and skills!</p>
+      </div>
+
+      <div>
+        <h2>Project overView</h2>
+        <ul>
+          <li>Project goals: create a react ts app with a simple CRUD and make it sexy</li>
+          <li>Css will be tailwind</li>
+          <li>Back-end will be done with Firebase but at some time i will make a fullstack aplication</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li>Project 1: Dashboard za praćenje učenja/projekata</li>
+          <li>Project 2: Pomodoro timer</li>
+          <li>Project 3: Interaktivna mapa</li>
+          <li>Project 4: E-commerce Site</li>
+          <li>Project 5: Quiz aplikacija</li>
+          <li>Project 6: Blog</li>
+        </ul>
+      </div>
+
+    <footer>
+      <p>©2025 Filip Todorović. All rights reserved.</p>
+    </footer>
+    </div>
+  )
+}
+
+export default App
